e618e3f93e808e41f724d04f7ff82d2ec15f11f8
Oleks <aleks.kontny@gmail.com>
2024-06-05 20:46:01 +0200

    initial commit

---
+++ src/main/java/mosbach/dhbw/cftestbackend/CFtestBackendApplication.java
@@ -0,0 +1,13 @@
+package mosbach.dhbw.cftestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CFtestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CFtestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mosbach/dhbw/cftestbackend/CFtestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mosbach.dhbw.cftestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CFtestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
